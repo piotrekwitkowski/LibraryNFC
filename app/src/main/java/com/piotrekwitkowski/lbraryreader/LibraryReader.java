73d9b7f9062d4d2be63b80d4f848871ed9f9ed9b
Piotr Witkowski <piotrekwitkowski1@gmail.com>
2020-03-21 00:30:04 +0100

    feat: HCE or DESFire check

--- app/src/main/java/com/piotrekwitkowski/lbraryreader/LibraryReader.java
+++ app/src/main/java/com/piotrekwitkowski/lbraryreader/LibraryReader.java
@@ -8,16 +8,33 @@ import java.io.IOException;
 class LibraryReader {
     private String TAG = "LibraryReader";
 
+    // TODO: change ID to F...
+    // see: https://stackoverflow.com/questions/27533193/android-hce-are-there-rules-for-aid
+    private final String HCE_AID = "A0000002471001";
+
     void processTag(Tag tag) {
         Log.i(TAG, "processTag called");
         IsoDep isoDep = IsoDep.get(tag);
         try {
             isoDep.connect();
-            byte[] response = isoDep.transceive(ByteUtils.hexStringToByteArray("00A4040007A0000002471001"));
-            Log.i(TAG, "response: " + ByteUtils.byteArrayToHexString(response));
+
+            getDesfireMasterFile(isoDep);
+
             isoDep.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    private void getDesfireMasterFile(IsoDep isoDep) throws IOException {
+        Log.i(TAG, "getDesfireMasterFile");
+        byte[] response = isoDep.transceive(ByteUtils.hexStringToByteArray("00A4040007" + HCE_AID));
+
+        if (ByteUtils.byteArrayToHexString(response).equals("9000")) {
+            Log.i(TAG, "it was HCE!");
+        } else {
+            Log.i(TAG, "it was DESFire card!");
+        }
+    }
+
 }
