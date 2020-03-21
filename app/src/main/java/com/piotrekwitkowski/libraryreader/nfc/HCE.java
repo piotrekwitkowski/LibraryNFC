3039e8b26797e30b5e9a4fe75d6b47d723788276
Piotr Witkowski <piotrekwitkowski1@gmail.com>
2020-03-22 00:08:56 +0100

    fix: return type

--- app/src/main/java/com/piotrekwitkowski/libraryreader/nfc/HCE.java
+++ app/src/main/java/com/piotrekwitkowski/libraryreader/nfc/HCE.java
@@ -14,7 +14,7 @@ public class HCE {
     // see: https://stackoverflow.com/questions/27533193/android-hce-are-there-rules-for-aid
     private static final AID HCE_APP = new AID("A0000002471001");
 
-    public static byte[] selectAndroidApp(IsoDep isoDep) throws IOException {
+    public static Response selectAndroidApp(IsoDep isoDep) throws IOException {
         Log.i(TAG, "selectAndroidApp()");
         byte[] commandApdu = wrapApdu((byte) 0x00, SELECT, (byte) 0x04, (byte) 0x00, HCE_APP.getAid());
         return isoDep.transceive(commandApdu);
