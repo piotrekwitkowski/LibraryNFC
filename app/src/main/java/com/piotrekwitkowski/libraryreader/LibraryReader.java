2425f1beec8432ff0a5f4a759789c8cbd303311a
Piotr Witkowski <piotrekwitkowski1@gmail.com>
2020-03-21 21:08:48 +0100

    refactor: LibraryReader fields

--- app/src/main/java/com/piotrekwitkowski/libraryreader/LibraryReader.java
+++ app/src/main/java/com/piotrekwitkowski/libraryreader/LibraryReader.java
@@ -11,22 +11,21 @@ import java.util.Arrays;
 
 class LibraryReader {
     private static final String TAG = "LibraryReader";
-    private static final AID LIBRARY_AID = new AID("015548");
-
-    private static final AESKey LIBRARY_AES_KEY = new AESKey("00000000000000000000000000000000");
-    private static final byte LIBRARY_KEY_NUMBER = (byte) 0x00;
-    private static final byte LIBRARY_FILE_NUMBER = (byte) 0x00;
-    private static final byte[] LIBRARY_FILE_OFFSET = new byte[] {(byte) 0x00, (byte) 0x00,(byte) 0x00};
-    private static final byte[] LIBRARY_FILE_LENGTH = new byte[] {(byte) 0x00, (byte) 0x00,(byte) 0x00};
+    private static final AID AID = new AID("015548");
+    private static final AESKey AES_KEY = new AESKey("00000000000000000000000000000000");
+    private static final byte KEY_NUMBER = (byte) 0x00;
+    private static final byte FILE_NUMBER = (byte) 0x00;
+    private static final byte[] FILE_OFFSET = new byte[] {(byte) 0x00, (byte) 0x00,(byte) 0x00};
+    private static final byte[] FILE_LENGTH = new byte[] {(byte) 0x00, (byte) 0x00,(byte) 0x00};
 
     void processTag(Tag tag) {
         Log.i(TAG, "processTag()");
         IsoDep isoDep = IsoDep.get(tag);
         try {
             StudentId studentId = StudentId.getStudentId(isoDep);
-            studentId.selectApplication(LIBRARY_AID);
-            byte[] sessionKey = studentId.authenticateAES(LIBRARY_AES_KEY, LIBRARY_KEY_NUMBER);
-            byte[] file = studentId.getFile(LIBRARY_FILE_NUMBER, LIBRARY_FILE_OFFSET, LIBRARY_FILE_LENGTH);
+            studentId.selectApplication(AID);
+            byte[] sessionKey = studentId.authenticateAES(AES_KEY, KEY_NUMBER);
+            byte[] file = studentId.getFile(FILE_NUMBER, FILE_OFFSET, FILE_LENGTH);
             byte[] libraryId = Arrays.copyOfRange(file, 11, 22);
             Log.i(TAG, "libraryId: " + new String(libraryId));
 
