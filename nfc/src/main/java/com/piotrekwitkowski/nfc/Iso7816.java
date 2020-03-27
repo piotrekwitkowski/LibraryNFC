12d7e926e9b225c89bfc3105b5aedd11003bc80a
Piotr Witkowski <piotrekwitkowski1@gmail.com>
2020-03-27 23:24:35 +0100

    refactor: SELECT

--- nfc/src/main/java/com/piotrekwitkowski/nfc/Iso7816.java
+++ nfc/src/main/java/com/piotrekwitkowski/nfc/Iso7816.java
@@ -3,7 +3,7 @@ package com.piotrekwitkowski.nfc;
 public class Iso7816 {
     public static final byte[] RESPONSE_SUCCESS = new byte[] {(byte) 0x90, (byte) 0x00};
     public static final byte[] RESPONSE_INTERNAL_ERROR = new byte[] {(byte) 0x90, (byte) 0x00};
-    private static final byte ISO7816_SELECT_COMMAND = (byte) 0xA4;
+    private static final byte SELECT = (byte) 0xA4;
 //    private final static byte READ_BINARY = (byte) 0xB0;
 //    private final static byte UPDATE_BINARY = (byte) 0xD6;
 //    private final static byte READ_RECORDS = (byte) 0xB2;
@@ -13,7 +13,7 @@ public class Iso7816 {
 //    private final static byte EXTERNAL_AUTHENTICATE = (byte) 0x82;
 
     static byte[] wrapApdu(byte[] command) {
-        byte[] apduRequiredPart = new byte[] {(byte) 0, ISO7816_SELECT_COMMAND, (byte) 4, (byte) 0};
+        byte[] apduRequiredPart = new byte[] {(byte) 0, SELECT, (byte) 4, (byte) 0};
         if (command.length == 0) {
             return apduRequiredPart;
         } else {
